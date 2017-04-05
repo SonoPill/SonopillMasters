583ed8c90c9cc4dbb6eac45a42e79b8e2e08b28f
CoderCal <1604414@abertay.ac.uk>
2017-04-05 18:36:00 +0100

    Updating master branch to contain all assets. Inclusion of multiple location movement with basic button HUD. Applied Neils intestine design to the project - looks grusome

---
+++ SonoPillTest/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour 
+{
+	public float mouseSensitivity = 100.0f;
+	public float clampAngle = 80.0f;
+
+	private float rotY = 0.0f; //rotation of the y axis
+	private float rotX = 0.0f; //rotation of the x axis
+
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        *This IF is temporary, just for simplicity on the PC prototype for now
+        *This is to keep the camera steeady when the player just wants to press a button
+        */
+        if (Input.GetKey(KeyCode.LeftShift)){
+
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            rotY += mouseX * mouseSensitivity * Time.deltaTime;
+            rotX -= mouseY * mouseSensitivity * Time.deltaTime; //use minues equals so y axis not inverted
+
+            rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+            Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+            transform.rotation = localRotation;
+        }
+	}
+}
---
+++ SonoPillTest/Assets/Scripts/MoveToLocation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToLocation : MonoBehaviour
+{
+
+    public void MoveLocation(GameObject targetLocation)
+    {
+        // move THIS to the in game location of target
+        this.transform.position = targetLocation.transform.position;
+    }
+}
